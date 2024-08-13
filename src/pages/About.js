7d8299c98cf60f7334f7d04fe85742781edb74a6
Alexandr Kotenko <kpshmt@gmail.com>
2024-08-13 13:36:35 +0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,9 +7,9 @@ const About = () => {
     <div className={styles.container}>
       <div className={styles.logo} />
       <div className={styles.skewedBlock}>
-      <h1 className={styles.heading}>BONUS 120%</h1></div>
-      <h2 className={styles.bonusAmount}>700 TRY </h2>
-      <h2 className={styles.freeSpins}>+ 250 bedava dönüşler</h2>
+      <h1 className={styles.heading}>BONUSU 120%</h1></div>
+      <h2 className={styles.bonusAmount}>700 AZN </h2>
+      <h2 className={styles.freeSpins}>+ 250 free spins</h2>
       <a href='/about'><button className={styles.button}>Bonus kazanın</button> </a>
     </div>
   );
